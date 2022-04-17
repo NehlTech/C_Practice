38ea1477630f9f71a0b53c62afa56dcb87eb4d3e
NhelTech <topmoonit@Bryts-MacBook-Pro.local>
2022-04-17 04:27:18 +0000

    Rectangle

---
+++ Rectangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    double width = 32.3;
+    double height = 15.3;
+    double perimeter = 0.0;
+    double area = 0.0;
+
+    printf("Perimeter of rectangle is %0.1fcm\n", 2 * (width + height));
+    printf("Area of rectangle is %0.1f cm^2", width*height);
+
+    return 0;
+
+
+}
--- arithmetic_operation.c
+++ arithmetic_operation.c
@@ -2,7 +2,7 @@
 
 int main(){
     int a = 2, b = 3, c = 4, d = 5;
-    printf("a * b/c = %d\n", a *b/c);
+    printf("a * b/c = %d\n", a*b/c);
     printf("a + b - c = %d\n", a+b/-c);
     printf("a + b * d - c %% a = %d", a+b*d-c%a);
 
